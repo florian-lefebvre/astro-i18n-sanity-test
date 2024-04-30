2af910a208e7dfdc982f73e592624888b00dcaf9
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-30 16:09:57 +0200

    feat: setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import i18n from "@astrolicious/i18n";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [i18n({
+    defaultLocale: "en",
+    locales: ["en", "fr"]
+  }), sanity({
+    projectId: "TODO:",
+    dataset: "TODO:",
+    useCdn: false
+  })]
+});
